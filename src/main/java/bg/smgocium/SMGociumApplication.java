88b29ac4641b3ea2aa840ebf237245e5f9a3bb5d
Endeville <victor.popdonchev@gmail.com>
2023-01-31 22:26:25 +0200

    Initial commit: -application.yml -build.gradle

---
+++ src/main/java/bg/smgocium/SMGociumApplication.java
@@ -0,0 +1,13 @@
+package bg.smgocium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMGociumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SMGociumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/smgocium/smgocium/SmGociumApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.smgocium.smgocium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmGociumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
